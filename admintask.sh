2e8963fef502f0243b8656ce01a5669bee4d8cd7
romankolivashko <44418240+romankolivashko@users.noreply.github.com>
2020-04-08 11:57:16 -0700

    Create admintask.sh

---
+++ admintask.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Purpose: Basic admin tasks
+# Date: 4/8/2020
+
+echo
+echo This script will run basic admin sommands
+echo
+top | head -10
+echo
+df -h
+echo
+free -m
+echo
+uptime
+echo
+iostat
+echo
+echo End of script
